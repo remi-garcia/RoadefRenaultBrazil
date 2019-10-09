2577ae2e01b9d037b08d4830833712103c189494
k-fretaud <49268413+k-fretaud@users.noreply.github.com>
2019-10-09 22:07:22 +0200

    Add header

--- src/vns.jl
+++ src/vns.jl
@@ -1,3 +1,32 @@
+#=
+# VNS for the RENAULT Roadef 2005 challenge
+# inspired by work of
+# Celso C. Ribeiro, Daniel Aloise, Thiago F. Noronha, Caroline Rocha, Sebastián Urrutia
+#
+# @Author Boualem Lamraoui, Benoît Le Badezet, Benoit Loger, Jonathan Fontaine, Killian Fretaud, Rémi Garcia
+=#
+
+
+
+# Complete strategy can be summerize as follow :
+#
+#   (1) Use a constructive heuristic to find an initial solution.
+#   (2) Use an improvement heuristic to optimize the first objective.
+#   (3) Use an improvement heuristic to optimize the second objective.
+#   (4) Use a repair heuristic to make the current solution feasible.
+#   (5) Use an improvement heuristic to optimize the third objective.
+#
+
+
+
+
+##===================================================##
+##                Algorithme ILS                     ##
+##===================================================##
+
+
+
+
 function ILS(s)
     S = copy(s)
     cond = false #TODO

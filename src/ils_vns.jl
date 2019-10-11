8ce37a5b379fcc4ff063b62d2ba78bdbc496683d
k-fretaud <49268413+k-fretaud@users.noreply.github.com>
2019-10-11 11:20:33 +0200

    Memo for data struct

--- src/ils_vns.jl
+++ src/ils_vns.jl
@@ -18,6 +18,28 @@
 #
 
 
+
+##===================================================##
+##                 Data stuctures                    ##
+##===================================================##
+
+# vector of cars (sequence pi or s in the following algorithms)
+s = [1 2 3 4 5 6 7 8 9 10] # size n
+# Three matrix m*n (rows = options, columns = cars)
+M1 = []
+    # M1_ij is the number of cars with oj in the subsequence of
+    # q(oj) cars starting at position i of sequence pi
+M2 = []
+    # M2_ij is the number of subsequences of q(oj) cars starting
+    # at positions 1 up to i in which the number of cars that
+    # require oj is greater than p(oj)
+M3 = []
+    # M3_ij is the number of subsequences in which the number of
+    # cars that require oj is greater than or equal to p(oj)
+
+
+
+
 ##===================================================##
 ##                Greedy algorithms                  ##
 ##===================================================##

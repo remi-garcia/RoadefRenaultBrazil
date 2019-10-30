2a81176777e0a22640b3e9cb77abcaaa14748e8b
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-10-30 18:07:13 +0100

    Draft of moves function

---
+++ src/functions.jl
@@ -0,0 +1,16 @@
+#=
+# This file has to purpose to have functions that are used in different algorithm.
+#
+# @Author Boualem Lamraoui, Benoît Le Badezet, Benoit Loger, Jonathan Fontaine, Killian Fretaud, Rémi Garcia
+# =#
+
+include("solution.jl")
+include("parser.jl")
+
+function move_exchange(solution::Solution, i::Int, j::Int, instance::Instances)
+    # TODO
+end
+
+function move_insertion(solution::Solution, i::Int, j::Int, instance::Instances)
+    # TODO
+end

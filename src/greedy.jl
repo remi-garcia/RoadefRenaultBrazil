6c9ccdeec76e98bcac7b30813522d97b3ea866ef
Rémi <47191601+remi-garcia@users.noreply.github.com>
2019-10-31 13:54:44 +0100

    start commenting and replace sol by solution for consistency

--- src/greedy.jl
+++ src/greedy.jl
@@ -2,13 +2,17 @@
 This file contains all function used to construct a first valid solution.
 =#
 
+"""
+    update_late_violation!(solution::Solution, nb::Int, last::Int,
+                           p::Array{Int, 1}, q::Array{Int, 1},
+                           flag::Array{Bool, 2}, shift::Int = 0)
 
-function update_late_violation!(    sol::Solution,
-                                    nb::Int, # number of options treated
-                                    last::Int,
-                                    p::Array{Int, 1}, q::Array{Int, 1},
-                                    flag::Array{Bool, 2},
-                                    shift::Int=0   #= for LPRC =#       )
+Update `solution.M1`, `solution.M2` and `solution.M3` with cars from the day
+before. `shift` parameter is used for LPRC.
+"""
+function update_late_violation!(solution::Solution, nb::Int, last::Int,
+                                p::Array{Int, 1}, q::Array{Int, 1},
+                                flag::Array{Bool, 2}, shift::Int = 0)
     # For the nb_late_prec_day first cars
     # Update M1, M2, M3
     for j in 1:nb
@@ -17,21 +21,26 @@ function update_late_violation!(    sol::Solution,
         # first sequence is compute wihout smart thoughts
         for i in 1:q[j]
             if flag[i,j] # if i has the option, M1 increments
-                sol.M1[J,1] = sol.M1[J,1] + 1
+                solution.M1[J,1] = solution.M1[J,1] + 1
             end
         end
         # First column of M2 and M3 can be update
-        sol.M2[J,1] = (sol.M1[J,1] >  p[j] ? 1 : 0)
-        sol.M3[J,1] = (sol.M1[J,1] >= p[j] ? 1 : 0)
+        solution.M2[J,1] = (solution.M1[J,1] >  p[j] ? 1 : 0)
+        solution.M3[J,1] = (solution.M1[J,1] >= p[j] ? 1 : 0)
 
-        update_sol!(sol, nb, 2, last, p, q, flag, shift)
+        update_solution!(solution, nb, 2, last, p, q, flag, shift)
     end
+
+    return solution
 end
 
+"""
 
+
+"""
 # Parameter "first" seems now useless, I thought it would allow us to use the
 # function during the next phase (updating solution after pushing a new car)
-function update_sol!(   sol::Solution,
+function update_solution!(   solution::Solution,
                         nb::Int, # number of options treated
                         first::Int, last::Int,
                         p::Array{Int, 1}, q::Array{Int, 1},
@@ -41,31 +50,31 @@ function update_sol!(   sol::Solution,
         J = j + shift
         # for each shift of sequences
         for i in first:last
-            sol.M1[J,i] = sol.M1[J,i-1]
+            solution.M1[J,i] = solution.M1[J,i-1]
 
             # previous case had flag -> not in anymore
-            I = sol.sequence[i-1]
+            I = solution.sequence[i-1]
             if flag[I,j]
-                sol.M1[J,i] = sol.M1[J,i] - 1
+                solution.M1[J,i] = solution.M1[J,i] - 1
             end
 
             # new case has flag -> in now
             if (i+q[j]-1) <= last
-                I = sol.sequence[(i+q[j])-1]
+                I = solution.sequence[(i+q[j])-1]
                 if flag[I,j]
-                    sol.M1[J,i] = sol.M1[J,i] + 1
+                    solution.M1[J,i] = solution.M1[J,i] + 1
                 end
             end
 
             # First column of M2 and M3 can be update
-            sol.M2[J,i] = sol.M2[J,i-1] + (sol.M1[J,i] >  p[j] ? 1 : 0)
-            sol.M3[J,i] = sol.M3[J,i-1] + (sol.M1[J,i] >= p[j] ? 1 : 0)
+            solution.M2[J,i] = solution.M2[J,i-1] + (solution.M1[J,i] >  p[j] ? 1 : 0)
+            solution.M3[J,i] = solution.M3[J,i-1] + (solution.M1[J,i] >= p[j] ? 1 : 0)
         end
     end
 end
 
 
-function update_sol_atpos!( sol::Solution,
+function update_sol_atpos!( solution::Solution,
                             nb::Int,
                             pos::Int,
                             p::Array{Int, 1}, q::Array{Int, 1},
@@ -74,21 +83,21 @@ function update_sol_atpos!( sol::Solution,
     for j in 1:nb
         J = j + shift
         # for each shift of sequence reaching this position
-        I = sol.sequence[pos]
+        I = solution.sequence[pos]
         for i in (pos - q[j] + 1):pos
 
             # new car has option j -> update M1
             if flag[I,j]
-                sol.M1[J,i] = sol.M1[J,i] + 1
+                solution.M1[J,i] = solution.M1[J,i] + 1
             end
 
             # First column of M2 and M3 can be update
             if i == 1
-                sol.M2[J,i] = 0 + (sol.M1[J,i] >  p[j] ? 1 : 0)
-                sol.M3[J,i] = 0 + (sol.M1[J,i] >= p[j] ? 1 : 0)
+                solution.M2[J,i] = 0 + (solution.M1[J,i] >  p[j] ? 1 : 0)
+                solution.M3[J,i] = 0 + (solution.M1[J,i] >= p[j] ? 1 : 0)
             else
-                sol.M2[J,i] = sol.M2[J,i-1] + (sol.M1[J,i] >  p[j] ? 1 : 0)
-                sol.M3[J,i] = sol.M3[J,i-1] + (sol.M1[J,i] >= p[j] ? 1 : 0)
+                solution.M2[J,i] = solution.M2[J,i-1] + (solution.M1[J,i] >  p[j] ? 1 : 0)
+                solution.M3[J,i] = solution.M3[J,i-1] + (solution.M1[J,i] >= p[j] ? 1 : 0)
             end
         end
     end
@@ -99,22 +108,22 @@ function greedy(inst::Instances)
     # The constructive greedy heuristic starts with a partial sequence formed
     # by the remaining cars from the previous day. We compute an empty sequence
     # with some cars already scheduled
-    sol = init_solution(inst)
+    solution = init_solution(inst)
     # We have V the set of cars to be scheduled
-    len = (sol.n) - (inst.nb_late_prec_day)
-    V = collect((inst.nb_late_prec_day+1):(sol.n))
+    len = (solution.n) - (inst.nb_late_prec_day)
+    V = collect((inst.nb_late_prec_day+1):(solution.n))
     nbH = inst.nb_HPRC
 
     # For the nb_late_prec_day first cars
     # Update M1, M2, M3
     # TODO: clean parameters
-    update_late_violation!(sol, inst.nb_HPRC, inst.nb_late_prec_day, inst.HPRC_p, inst.HPRC_q, inst.HPRC_flag)
-    update_late_violation!(sol, inst.nb_LPRC, inst.nb_late_prec_day, inst.LPRC_p, inst.LPRC_q, inst.LPRC_flag, inst.nb_HPRC)
+    update_late_violation!(solution, inst.nb_HPRC, inst.nb_late_prec_day, inst.HPRC_p, inst.HPRC_q, inst.HPRC_flag)
+    update_late_violation!(solution, inst.nb_LPRC, inst.nb_late_prec_day, inst.LPRC_p, inst.LPRC_q, inst.LPRC_flag, inst.nb_HPRC)
 
     # The greedy criterion consists in choosing, at each iteration, the car
     # that induces the smallest number of new violations when inserted at
     # the end of the current partial sequence.
-    for pos in (inst.nb_late_prec_day+1):(sol.n)
+    for pos in (inst.nb_late_prec_day+1):(solution.n)
 
         # Compute the number of violations caused by each car
         nb_new_violation = zeros(Int, len)
@@ -122,7 +131,7 @@ function greedy(inst::Instances)
             for j in 1:inst.nb_HPRC
                 if inst.HPRC_flag[V[c],j]
                     for i in ((pos - inst.HPRC_q[j])+1):pos
-                        if sol.M1[j,i] >= inst.HPRC_p[j]
+                        if solution.M1[j,i] >= inst.HPRC_p[j]
                             nb_new_violation[c] = nb_new_violation[c] + 1
                         end
                     end
@@ -172,14 +181,14 @@ function greedy(inst::Instances)
         end
 
         c = candidates[1]     # We have a valid candidate
-        sol.sequence[pos] = c
+        solution.sequence[pos] = c
         len = len - 1
         filter!(x->x≠c, V)    # The car is not in the list anymore
 
         # Update M1, M2 and M3
-        update_sol_atpos!(sol, inst.nb_HPRC, pos, inst.HPRC_p, inst.HPRC_q, inst.HPRC_flag)
-        update_sol_atpos!(sol, inst.nb_LPRC, pos, inst.LPRC_p, inst.LPRC_q, inst.LPRC_flag, inst.nb_HPRC)
+        update_sol_atpos!(solution, inst.nb_HPRC, pos, inst.HPRC_p, inst.HPRC_q, inst.HPRC_flag)
+        update_sol_atpos!(solution, inst.nb_LPRC, pos, inst.LPRC_p, inst.LPRC_q, inst.LPRC_flag, inst.nb_HPRC)
     end
 
-    return sol
+    return solution
 end

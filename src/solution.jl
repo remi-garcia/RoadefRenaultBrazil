7513ffbcf68fabf7aee0b7dd25aed4a143142360
Rémi <47191601+remi-garcia@users.noreply.github.com>
2019-10-31 17:20:12 +0100

    add compute_Ms()

--- src/solution.jl
+++ src/solution.jl
@@ -1,5 +1,5 @@
 mutable struct Solution
-    n:: Int
+    n::Int
         # Size
     sequence::Array{Int, 1}
         # vector of cars (sequence pi or s in the following algorithms)
@@ -16,19 +16,27 @@ mutable struct Solution
 
     Solution(nC::Int,nO::Int) = new(
         nC,
-        zeros(Int,nC),#collect(1:nC),
+        zeros(Int,nC),
         zeros(Int,nO,nC),
         zeros(Int,nO,nC),
         zeros(Int,nO,nC)
     )
 end
 
-
-# Build an initial
+"""
+TODO
+"""
 function init_solution(nom_fichier::String, type_fichier::String)
     # Read in data files
     instance = parser(nom_fichier, type_fichier)
 
+    return init_solution(instance)
+end
+
+"""
+TODO
+"""
+function init_solution(instance::Instance)
     n = length(instance.color_code)
     m = instance.nb_HPRC + instance.nb_LPRC # number of ratio
     solution = Solution(n,m)
@@ -36,17 +44,55 @@ function init_solution(nom_fichier::String, type_fichier::String)
     for i in 1:instance.nb_late_prec_day
         solution.sequence[i] = i
     end
+
     return solution
 end
 
-# Build an initial
-function init_solution(instance::Instances)
-    n = length(instance.color_code)
-    m = instance.nb_HPRC + instance.nb_LPRC # number of ratio
-    solution = Solution(n,m)
+"""
+    compute_Ms!(solution::Solution)
 
-    for i in 1:instance.nb_late_prec_day
-        solution.sequence[i] = i
+Computes `solution.M1`, `solution.M2` and `solution.M3` given
+`solution.sequence` and an instance.
+"""
+function compute_Ms!(solution::Solution, instance::Instance)
+    n = length(solution.sequence)
+    m = instance.nb_HPRC + instance.nb_LPRC # number of ratio
+    solution.M1 = zeros(Int, m, n)
+    solution.M2 = zeros(Int, m, n)
+    solution.M3 = zeros(Int, m, n)
+    # Update M1, M2 and M3 for the first car
+    for j in 1:m
+        for i in 1:instance.RC_q[j]
+            if instance.flag[solution.sequence[i],j]
+                solution.M1[j, 1] = 1
+            end
+        end
+        # First column of M2 and M3 can be update
+        solution.M2[j, 1] = (solution.M1[j, 1] >  instance.RC_p[j] ? 1 : 0)
+        solution.M3[j, 1] = (solution.M1[j, 1] >= instance.RC_p[j] ? 1 : 0)
+
+        # for each shift of sequence
+        for i in 2:n
+            current_i = solution.sequence[i]
+            precedent_i = solution.sequence[i-1]
+            solution.M1[j, current_i] = solution.M1[j, precedent_i]
+            # previous case had flag -> not in anymore
+
+            if instance.flag[precedent_i,j]
+                solution.M1[j, current_i] = solution.M1[j, current_i] - 1
+            end
+            # new case has flag -> in now
+            if (i + q[j] - 1) <= n
+                other_i = solution.sequence[(i + q[j]) - 1]
+                if instance.flag[other_i, j]
+                    solution.M1[J, current_i] = solution.M1[J, current_i] + 1
+                end
+            end
+            # First column of M2 and M3 can be update
+            solution.M2[J, current_i] = solution.M2[J, precedent_i] + (solution.M1[J, current_i] >  instance.RC_p[j] ? 1 : 0)
+            solution.M3[J, current_i] = solution.M3[J, precedent_i] + (solution.M1[J, current_i] >= instance.RC_p[j] ? 1 : 0)
+        end
     end
+
     return solution
 end

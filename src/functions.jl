6b8a73d8966b661bb4acbbbd661148ea8c8c30d9
Rémi <47191601+remi-garcia@users.noreply.github.com>
2019-10-31 15:05:30 +0100

    commenting

--- src/functions.jl
+++ src/functions.jl
@@ -1,18 +1,67 @@
 #=
-# This file has to purpose to have functions that are used in different algorithm.
+# This file contains functions that are used in VNS and ILS.
 #
 # @Author Boualem Lamraoui, Benoît Le Badezet, Benoit Loger, Jonathan Fontaine, Killian Fretaud, Rémi Garcia
 # =#
 
-include("solution.jl")
-include("parser.jl")
+"""
+    move_exchange!(solution::Solution, i::Int, j::Int, instance::Instances)
 
-function move_exchange(solution::Solution, i::Int, j::Int, instance::Instances)
+Interverts the car `i` with the car `j` in `solution.sequence`. Updates
+`solution.M1`, `solution.M2` and `solution.M3`.
+"""
+function move_exchange!(solution::Solution, i::Int, j::Int, instance::Instances)
     # TODO
-    return Solution(1,1)
+    return Solution
 end
 
-function move_insertion(solution::Solution, i::Int, j::Int, instance::Instances)
+"""
+    move_insertion!(solution::Solution, i::Int, j::Int, instance::Instances)
+
+Inserts the car `i` before the car `j` in `solution.sequence`. Updates
+`solution.M1`, `solution.M2` and `solution.M3`.
+"""
+function move_insertion!(solution::Solution, i::Int, j::Int, instance::Instances)
     # TODO
-    return Solution(1,1)
+    return Solution
+end
+
+"""
+    cost_move_exchange(solution::Solution, i::Int, j::Int,
+                       instance::Instances, objective::Int)
+
+Return the cost of the exchange of the car `i` with the car `j` with respect to
+objective `objective`. A negative cost means that the move is interesting with
+respect to objective `objective`.
+"""
+function cost_move_exchange(solution::Solution, i::Int, j::Int,
+                            instance::Instances, objective::Int)
+    #TODO it might be important that objective is a vector of Int, then we could
+    #return a vector of cost.
+
+    # objective should take values between 1 and 3.
+    @assert objective >= 1
+    @assert objective <= 3
+    # TODO
+    return 0
+end
+
+"""
+    cost_move_insertion(solution::Solution, i::Int, j::Int,
+                        instance::Instances, objective::Int)
+
+Return the cost of the insertion of the car `i` before the car `j` with respect
+to objective `objective`. A negative cost means that the move is interesting
+with respect to objective `objective`.
+"""
+function cost_move_insertion(solution::Solution, i::Int, j::Int,
+                             instance::Instances, objective::Int)
+    #TODO it might be important that objective is a vector of Int, then we could
+    #return a vector of cost.
+
+    # objective should take values between 1 and 3.
+    @assert objective >= 1
+    @assert objective <= 3
+    # TODO
+    return 0
 end

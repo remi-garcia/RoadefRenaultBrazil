7c4b82f00af5eb657e77632625c00644ba5ed9f8
boualem <32416754+melauob@users.noreply.github.com>
2019-11-02 23:11:08 +0100

    Implement move_exchange! (#25)
    * swap()
    
    * swap after some modification
    
    * .
    
    * finally something that works
    
    * Clean the merge
    
    * Clean spacing
    
    * Adapt after changes in parser
    
    * correct index
    
    * This need to be merged

--- src/functions.jl
+++ src/functions.jl
@@ -13,8 +13,51 @@ Interverts the car `i` with the car `j` in `solution.sequence`. Updates
 `solution.M1`, `solution.M2` and `solution.M3`.
 """
 function move_exchange!(solution::Solution, i::Int, j::Int, instance::Instance)
-    # TODO
-    return Solution
+    solution.sequence[i], solution.sequence[j] = solution.sequence[j], solution.sequence[i]
+    update_matrices!(solution, solution.n, instance)
+    # for k in 1:(instance.nb_HPRC + instance.nb_LPRC)
+    #     if xor(instance.RC_flag[i, k], instance.RC_flag[j, k])
+    #         plusminusone = 1
+    #         if instance.RC_flag[i, k]
+    #             plusminusone = -1
+    #         end
+    #         pos1 = i - (instance.RC_q[k] - 1)
+    #         if pos1 < 1
+    #             pos1 = 1
+    #         end
+    #         for l in pos1:i
+    #             solution.M1[k, l] += plusminusone
+    #         end
+    #         pos2 = j - (instance.RC_q[k] - 1)
+    #         if pos2 < 1
+    #             pos2 = 1
+    #         end
+    #         for l in pos2:j
+    #             solution.M1[k, l] -= plusminusone
+    #         end
+    #         pos1 -= 1
+    #         if pos1 < 1
+    #             pos1 = 1
+    #         end
+    #         cpt1 = solution.M2[k, pos1]
+    #         cpt2 = solution.M3[k, pos1]
+    #         for l in pos1:solution.n
+    #             if solution.M1[k, l] > instance.RC_p[k]
+    #                 cpt1 += 1
+    #                 solution.M2[k, l] = cpt1
+    #             else
+    #                 solution.M2[k, l] = cpt1
+    #             end
+    #             if solution.M1[k, l] >= instance.RC_p[k]
+    #                 cpt2 += 1
+    #                 solution.M3[k, l] = cpt2
+    #             else
+    #                 solution.M3[k, l] = cpt2
+    #             end
+    #         end
+    #     end
+    # end
+    return solution
 end
 
 """

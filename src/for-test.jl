80d11d6336894c14121e31a86193c457e0bc16ed
k-fretaud <49268413+k-fretaud@users.noreply.github.com>
2019-11-22 22:23:42 +0100

    Clean for-test.jl

--- src/for-test.jl
+++ src/for-test.jl
@@ -7,8 +7,6 @@
 #-------------------------------------------------------------------------------
 
 
-
-
 """
     TEST_move_exchange!(solution::Solution, car_pos_a::Int, car_pos_b::Int, instance::Instance)
 
@@ -22,26 +20,10 @@ function TEST_move_exchange!(solution::Solution, car_pos_a::Int, car_pos_b::Int,
         return solution
     end
     solution.sequence[car_pos_a], solution.sequence[car_pos_b] = solution.sequence[car_pos_b], solution.sequence[car_pos_a]
-
     update_matrices!(solution, instance.nb_cars, instance)
+
     return solution
 end
-
-
-
-
-
-#IN MAIN - test something like :
-# for i in instance.nb_late_prec_day:(instance.nb_cars-1)
-#     for j in i:instance.nb_cars
-#         c1 = cost_move_exchange(solution, i, j, instance, 3)
-#         c2 = TEST_cost_move_exchange(solution, i, j, instance, 3)
-#         if ((c1[1] != c2[1]) || (c1[2] != c2[2]))
-#             println("FAIL : ", c1, " vs. ", c2, " \t\t - exchange between ", i, " and ", j)
-#         end
-#     end
-# end
-
 
 """
     TEST_cost_move_exchange(solution::Solution, car_pos_a::Int, car_pos_b::Int,
@@ -51,19 +33,11 @@ Bad complexity version for function cost_move_exchange.
 """
 function TEST_cost_move_exchange(solution::Solution, car_pos_a::Int, car_pos_b::Int,
                             instance::Instance, objective::Int)
-
     s = deepcopy(solution)
     s = TEST_move_exchange!(s, car_pos_a, car_pos_b, instance)
 
     return cost(s, instance, objective) - cost(solution, instance, objective)
 end
-
-
-
-
-
-
-
 
 """
     TEST_move_insertion!(solution::Solution, old_index::Int, new_index::Int, instance::Instance)
@@ -72,12 +46,6 @@ Bad complexity version for function move_insertion.
 """
 function TEST_move_insertion!(solution::Solution, old_index::Int, new_index::Int, instance::Instance)
 
-    #Update sequence
-    # car = solution.sequence[old_index]
-    # deleteat!(solution.sequence, old_index)
-    # insert!(solution.sequence, new_index, car)
-
-    #Update sequence (better complexity ?)
     car_inserted = solution.sequence[old_index]
     if old_index < new_index
         for car_moved_pos in old_index:(new_index-1)
@@ -91,25 +59,10 @@ function TEST_move_insertion!(solution::Solution, old_index::Int, new_index::Int
         end
         solution.sequence[new_index] = car_inserted
     end
-
-    # TODO: COMPLEXITY NON-OPTIMAL
     update_matrices!(solution, instance.nb_cars, instance)
 
     return solution
 end
-
-
-
-#IN MAIN - test something like :
-# for i in instance.nb_late_prec_day:(instance.nb_cars-1)
-#     for j in i:instance.nb_cars
-#         c1 = cost_move_insertion(solution, i, j, instance, 3)
-#         c2 = TEST_cost_move_insertion(solution, i, j, instance, 3)
-#         if ((c1[1] != c2[1]) || (c1[2] != c2[2]))
-#             println("FAIL : ", c1, " vs. ", c2, " \t\t - exchange between ", i, " and ", j)
-#         end
-#     end
-# end
 
 """
     TEST_cost_move_insertion(solution::Solution, car_pos_a::Int,
@@ -121,7 +74,6 @@ function TEST_cost_move_insertion(solution::Solution, car_pos_a::Int,
                              instance::Instance, objective::Int)
 
     cost_on_objective = zeros(Int, instance.nb_cars, 3)
-
     for i in (instance.nb_late_prec_day+1):instance.nb_cars
         s = deepcopy(solution)
         s = TEST_move_insertion!(s, car_pos_a, i, instance)

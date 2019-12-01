5a58aa608fc9729e967bb7d4763bd4fb4a2fb611
Rémi Garcia <47191601+remi-garcia@users.noreply.github.com>
2019-12-01 19:12:10 +0100

    VNS and ILS modifications according to the original paper (#73)
    * Corrected VNS_LPRC

--- src/functions.jl
+++ src/functions.jl
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # File: functions.jl
-# Description: This file contains functions that are used in VNS and ILS.
+# Description: This file contains functions that are used in ILS and VNS.
 # Date: October 31, 2019
 # Author: Jonathan Fontaine, Killian Fretaud, Rémi Garcia,
 #         Boualem Lamraoui, Benoît Le Badezet, Benoit Loger
@@ -11,12 +11,99 @@ include("move_insertion.jl")
 include("move_exchange.jl")
 
 """
+    greedy_add!(solution::Solution, instance::Instance, k::Int, objectives::Int)
+
+Reinserts last car in the sequence of `solution`.
+"""
+function greedy_add!(solution::Solution, instance::Instance, k::Int, objectives::Int)
+    @assert objectives <= 3
+    @assert objectives >= 1
+    b0 = instance.nb_late_prec_day + 1
+    costs = cost_move_insertion(solution, instance.nb_cars, instance, objectives)
+    delta = weighted_sum(costs[b0, :])
+    best_delta = delta
+    best_position = b0
+    for position in (b0+1):(instance.nb_cars-k)
+        delta = weighted_sum(costs[position, :])
+        if delta < best_delta
+            best_delta = delta
+            best_position = position
+        end
+    end
+    move_insertion!(solution, instance.nb_cars, best_position, instance)
+
+    return solution
+end
+
+"""
+    find_critical_cars(solution::Solution, instance::Instance,
+                       objectives::BitArray{1})
+
+Returns the set cars involved in at least one HPRC violation and/or one LPRC
+violation.
+"""
+function find_critical_cars(solution::Solution, instance::Instance,
+                            objectives::BitArray{1})
+    @assert length(objectives) == 2
+    @assert true in objectives
+    if objectives[1]
+        first_option = 1
+    else
+        first_option = instance.nb_HPRC+1
+    end
+    if objectives[2]
+        last_option = instance.nb_HPRC+instance.nb_LPRC
+    else
+        last_option = instance.nb_HPRC
+    end
+
+    critical_cars = Set{Int}()
+    b0 = instance.nb_late_prec_day + 1
+    for index_car in b0:instance.nb_cars
+        for option in first_option:last_option
+            if solution.M1[option, index_car] > instance.RC_p[option]
+                index_car_lim = index_car + min(instance.RC_p[option], instance.nb_cars-index_car)
+                for index_car_add in index_car:index_car_lim
+                    if instance.RC_flag[solution.sequence[index_car_add], option]
+                        push!(critical_cars, index_car_add)
+                    end
+                end
+            end
+        end
+    end
+    return collect(critical_cars)
+end
+
+find_critical_cars(solution::Solution, instance::Instance, objectives::Int) =
+    find_critical_cars(solution::Solution, instance::Instance, [trues(objectives) ; falses(2-objectives)])
+
+"""
+    remove!(solution_init::Solution, instance::Instance,
+            k::Int, crit::Array{Int, 1})
+
+Removes `k` critical cars of the sequence of `solution_init`.
+"""
+function remove!(solution_init::Solution, instance::Instance,
+                 k::Int, crit::Array{Int, 1})
+    solution = deepcopy(solution_init)
+    indices = sort(randperm(length(crit))[1:k])
+    crit_sort = sort(crit, rev = true)
+    for i in 1:k
+        position = crit_sort[indices[i]]
+        move_insertion!(solution, position, instance.nb_cars, instance)
+    end
+
+    return solution
+end
+
+"""
     cost(solution::Solution, instance::Instance, objectives::BitArray{1})
 
 Returns the (partial) set of objective values (without weights) of `solution`.
 """
 function cost(solution::Solution, instance::Instance, objectives::BitArray{1})
     @assert length(objectives) == 3
+    @assert true in objectives
     cost_on_objective = zeros(Int, 3)
 
     if objectives[1]
@@ -47,27 +134,28 @@ function cost(solution::Solution, instance::Instance, objectives::BitArray{1})
 end
 
 """
-    cost(solution::Solution, instance::Instance, objective::Int)
+    cost(solution::Solution, instance::Instance, objectives::Int)
 
-Returns the set of unweighted values for objectives 1 to `objective` of `solution`.
+Returns the set of unweighted values for objectives 1 to `objectives` of `solution`.
 """
-cost(solution::Solution, instance::Instance, objective::Int) =
-    cost(solution::Solution, instance::Instance, [trues(objective) ; falses(3-objective)])
+cost(solution::Solution, instance::Instance, objectives::Int) =
+    cost(solution::Solution, instance::Instance, [trues(objectives) ; falses(3-objectives)])
+
+cost(solution::Solution, instance::Instance) =
+    cost(solution::Solution, instance::Instance, 3)
+
 
 """
     weighted_sum(cost_solution::Array{Int, 1})
 
 Returns the partial weighted sum of the objective values.
 """
-function weighted_sum(cost_solution::Array{Int, 1}, objective::Int)
-    # objective should take values between 1 and 3.
-    @assert objective >= 1
-    @assert objective <= 3
-    return sum(cost_solution[i] * WEIGHTS_OBJECTIVE_FUNCTION[i] for i in 1:objective)
+function weighted_sum(cost_solution::Array{Int, 1})
+    return sum(cost_solution[i] * WEIGHTS_OBJECTIVE_FUNCTION[i] for i in 1:length(cost_solution))
 end
 
-weighted_sum(solution::Solution, instance::Instance, objective::Int) =
-    weighted_sum(cost(solution, instance, objective), objective)
+weighted_sum(solution::Solution, instance::Instance) =
+    weighted_sum(cost(solution, instance))
 
 """
     print_cost(solution::Solution, instance::Instance)
@@ -79,7 +167,7 @@ function print_cost(solution::Solution, instance::Instance)
     println("\tHPRC violations: ", cost_solution[1])
     println("\tLPRC violations: ", cost_solution[2])
     println("\tPCC  violations: ", cost_solution[3])
-    println("\tObjective value is ",weighted_sum(cost_solution, 3))
+    println("\tObjective value is ", weighted_sum(cost_solution))
 end
 
 # RC functions
--- src/ils_hprc.jl
+++ src/ils_hprc.jl
@@ -11,60 +11,19 @@
 #-------------------------------------------------------------------------------
 
 """
-    remove!(solution_init::Solution, instance::Instance,
-            k::Int, crit::Array{Int, 1})
-
-Removes `k` critical cars of the sequence of `solution_init`.
-"""
-function remove!(solution_init::Solution, instance::Instance,
-                 k::Int, crit::Array{Int, 1})
-    solution = deepcopy(solution_init)
-    indices = sort(randperm(length(crit))[1:k])
-    crit_sort = sort(crit, rev = true)
-    for i in 1:k
-        position = crit_sort[indices[i]]
-        move_insertion!(solution, position, instance.nb_cars, instance)
-    end
-
-    return solution
-end
-
-"""
-    greedy_add!(solution::Solution, instance::Instance, k::Int)
-
-Reinserts last car in the sequence of `solution`.
-"""
-function greedy_add!(solution::Solution, instance::Instance, k::Int)
-    b0 = instance.nb_late_prec_day + 1
-    costs = cost_move_insertion(solution, instance.nb_cars, instance, 1)
-    delta = costs[b0]
-    posdelta = b0
-    for pos in b0+1:instance.nb_cars-k
-        if costs[pos] < delta
-            delta = costs[pos]
-            posdelta = pos
-        end
-    end
-    move_insertion!(solution, instance.nb_cars, posdelta, instance)
-
-    return solution
-end
-
-"""
     perturbation_ils_hprc(solution::Solution, instance::Instance,
-                          k::Int, crit::Array{Int,1})
+                          k::Int, critical_cars::Array{Int,1})
 
 Removes `k` cars of `solution` and inserts them back in the sequence.
 """
 function perturbation_ils_hprc(solution_init::Solution, instance::Instance,
-                               k::Int, crit::Array{Int,1})
+                               k::Int, critical_cars::Array{Int,1})
     solution = deepcopy(solution_init)
-    k = minimum([k, length(crit)])
-    remove!(solution, instance, k, crit)
+    k = min(k, length(critical_cars))
+    remove!(solution, instance, k, critical_cars)
     for i in 1:k
-        greedy_add!(solution, instance, k)
+        greedy_add!(solution, instance, k-i, 1)
     end
-
     return solution
 end
 
@@ -154,16 +113,16 @@ end
 
 """
     fast_local_search_exchange_ils_hprc!(solution::Solution, instance::Instance,
-                                         crit::Array{Int, 1}, start_time::UInt)
+                                         critical_cars::Array{Int, 1}, start_time::UInt)
 
 Performs a local search on `solution` using only exchange moves with respect to `instance`
-for well chosen cars tagged in `crit`.
+for well chosen cars tagged in `critical_cars`.
 """
 function fast_local_search_exchange_ils_hprc!(solution::Solution, instance::Instance,
-                                              crit::Array{Int, 1}, start_time::UInt)
-    while !isempty(crit) && (0.9 * TIME_LIMIT > (time_ns() - start_time) / 1.0e9)
+                                              critical_cars::Array{Int, 1}, start_time::UInt)
+    while !isempty(critical_cars) && (0.9 * TIME_LIMIT > (time_ns() - start_time) / 1.0e9)
         b0 = instance.nb_late_prec_day + 1      #First car of the current production day
-        position_car_a = rand(crit)
+        position_car_a = rand(critical_cars)
         hprc_current_car = HPRC_value(solution.sequence[position_car_a], instance)
         best_delta = 0
         new_possible_positions = Array{Int, 1}()
@@ -184,36 +143,13 @@ function fast_local_search_exchange_ils_hprc!(solution::Solution, instance::Inst
             move_exchange!(solution, position_car_a, k, instance)
             # Update critical cars:
             #TODO
-            deleteat!(crit, findfirst(isequal(position_car_a), crit))
+            deleteat!(critical_cars, findfirst(isequal(position_car_a), critical_cars))
         else
-            deleteat!(crit, findfirst(isequal(position_car_a), crit))
+            deleteat!(critical_cars, findfirst(isequal(position_car_a), critical_cars))
         end
     end
 
     return solution
-end
-
-"""
-    critical_cars_VNS_LPRC(solution::Solution, instance::Instance)
-
-Returns the set cars involved in at least one HPRC violation.
-"""
-function critical_cars_ILS_HPRC(solution::Solution, instance::Instance)
-    critical_car = Set{Int}()
-    b0 = instance.nb_late_prec_day+1
-    for index_car in b0:instance.nb_cars
-        for option in 1:instance.nb_HPRC
-            if solution.M1[option, index_car] > instance.RC_p[option]
-                index_car_lim = index_car + min(instance.RC_p[option], instance.nb_cars-index_car)
-                for index_car_add in index_car:index_car_lim
-                    if instance.RC_flag[solution.sequence[index_car_add], option]
-                        push!(critical_car, index_car_add)
-                    end
-                end
-            end
-        end
-    end
-    return collect(critical_car)
 end
 
 """
@@ -236,66 +172,66 @@ end
 
 """
 function restart_ils_hprc(solution_init::Solution, instance::Instance)
-    crit = critical_cars_ILS_HPRC(solution_init, instance)
-    solution = perturbation_ils_hprc(solution_init, instance, NBCAR_DIVERSIFICATION, crit)
+    critical_cars = find_critical_cars(solution_init, instance, 1)
+    solution = perturbation_ils_hprc(solution_init, instance, NBCAR_DIVERSIFICATION, critical_cars)
     return solution
 end
 
 """
-    ILS_HPRC(solution::Solution, instance::Instance, start_time::UInt)
+    ILS_HPRC(solution_init::Solution, instance::Instance, start_time::UInt)
 
-Main function of the ILS metaheuristic. Improves the `solution` on its first objective
+Main function of the ILS metaheuristic. Improves the `solution_init` on its first objective
 with respect to `instance`.
 """
-function ILS_HPRC(solution::Solution, instance::Instance, start_time::UInt)
+function ILS_HPRC(solution_init::Solution, instance::Instance, start_time::UInt)
     i = 0                               # Number of iterations since the last improvement
     nb_strong_perturbation = 0                      # Number of restarts done for a solution
-    s = deepcopy(solution)
-    s_opt = deepcopy(solution)
-    lastopt = deepcopy(solution)
+    solution = deepcopy(solution_init)
+    solution_opt = deepcopy(solution_init)
+    lastopt = deepcopy(solution_init)
     cond = 0 #TODO
-    while cond < STOPPING_CRITERIA_ILS_HPRC && cost_HPRC(s_opt, instance) != 0 && (0.9 * TIME_LIMIT > (time_ns() - start_time) / 1.0e9)
-        crit = critical_cars_ILS_HPRC(s, instance)
-        neighbor = perturbation_ils_hprc(s, instance, NBCAR_PERTURBATION, crit)
-        crit = critical_cars_ILS_HPRC(neighbor, instance)
-        if length(crit) > (instance.nb_cars * 0.6)
+    while cond < STOPPING_CRITERIA_ILS_HPRC && cost_HPRC(solution_opt, instance) != 0 && (0.9 * TIME_LIMIT > (time_ns() - start_time) / 1.0e9)
+        critical_cars = find_critical_cars(solution, instance, 1)
+        neighbor = perturbation_ils_hprc(solution, instance, NBCAR_PERTURBATION, critical_cars)
+        critical_cars = find_critical_cars(neighbor, instance, 1)
+        if length(critical_cars) > (instance.nb_cars * 0.6)
             neighbor = local_search_exchange_ils_hprc!(neighbor, instance, start_time)
         else
-            neighbor = fast_local_search_exchange_ils_hprc!(neighbor, instance, crit, start_time)
+            neighbor = fast_local_search_exchange_ils_hprc!(neighbor, instance, critical_cars, start_time)
         end
-        if cost_HPRC(s, instance) <= cost_HPRC(neighbor, instance)
-            s = neighbor
+        if cost_HPRC(solution, instance) <= cost_HPRC(neighbor, instance)
+            solution = neighbor
         end
         if i == ALPHA_ILS
-            s = intensification_ils_hprc!(s, instance, start_time)
+            solution = intensification_ils_hprc!(solution, instance, start_time)
         end
         if i == BETA_ILS
             cond = cond + 1
-            if cost_HPRC(lastopt, instance) > cost_HPRC(s_opt, instance)
-                lastopt = s_opt
+            if cost_HPRC(lastopt, instance) > cost_HPRC(solution_opt, instance)
+                lastopt = solution_opt
                 cond = 0
             end
-            if cost_HPRC(s, instance) > cost_HPRC(s_opt, instance)
-                s = s_opt                   # Restart from s*
+            if cost_HPRC(solution, instance) > cost_HPRC(solution_opt, instance)
+                solution = solution_opt                   # Restart from solution*
                 i = 0
             else
                 if nb_strong_perturbation < 3
-                    s = restart_ils_hprc(s, instance)       # Restart from strong perturbation (50 cars)
+                    solution = restart_ils_hprc(solution, instance)       # Restart from strong perturbation (50 cars)
                     nb_strong_perturbation = nb_strong_perturbation + 1
                     i = 0
                 else
-                    s = greedy(instance)                    # Regenerate an initial solution and restart
+                    solution = greedy(instance)                    # Regenerate an initial solution and restart
                     nb_strong_perturbation = 0
                     i = 0
                 end
             end
         end
-        if cost_HPRC(s, instance) < cost_HPRC(s_opt, instance)            # There is an improvement
-            s_opt = s
+        if cost_HPRC(solution, instance) < cost_HPRC(solution_opt, instance)            # There is an improvement
+            solution_opt = solution
             i = 0                   # So the number of iterations since the last improvement shall return to 0
         else
             i = i + 1
         end
     end
-    return s_opt
+    return solution_opt
 end

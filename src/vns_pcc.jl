6176642d88c5bbc7e7c0ac0822034e15c30102a4
Benoit <benoitloger@gmail.com>
2019-12-04 08:27:38 +0100

    adding some cases to avoid

--- src/vns_pcc.jl
+++ src/vns_pcc.jl
@@ -164,37 +164,41 @@ function repair!(solution::Solution, instance::Instance)
     end
 
     # Second strategy
+    println()
     first_violation = find_first_violation(solution, instance)
+    b0 = instance.nb_late_prec_day+1
     while first_violation != -1
         # Compute the best_insertion index
         solution_value = weighted_sum(solution, instance)
         cost_insertion = zeros(instance.nb_cars)
-        for i in 1:instance.nb_cars
-            if i <= instance.nb_late_prec_day || i == first_violation# Pour empêcher d'insérer dans les nb_late_prec_day
+        for i in b0:instance.nb_cars
+            if i <= instance.nb_late_prec_day || i == first_violation # avoiding insterting in sequence from the precedent day and at the same position
                 cost_insertion[i] = Inf
             else
-                #checking for validity of insertion
+                #checking for validity of insertion :  if it does not add an other violation of Paint batch constraint
                 batch_size = 1
                 batch_color = instance.color_code[solution.sequence[first_violation]]
-                j = 1
-                same_batch_sup = true
-                same_batch_inf = true
-                while same_batch_inf || same_batch_sup
-                    if instance.color_code[solution.sequence[i-j]] == batch_color && same_batch_inf
-                        batch_size += 1
-                    else
-                        same_batch_inf = false
-                    end
+                if instance.color_code[solution.sequence[i]] == batch_color # if inserting create a batch of length greater than one
+                    batch_size += 1
+                    j = 1
+                    same_batch_sup = true
+                    same_batch_inf = true
+                    while same_batch_inf || same_batch_sup
+                        if i-j > 0 && instance.color_code[solution.sequence[i-j]] == batch_color && same_batch_inf
+                            batch_size += 1
+                        else
+                            same_batch_inf = false
+                        end
 
-                    if i+j <= instance.nb_cars && instance.color_code[solution.sequence[i+j]] == batch_color && same_batch_inf
-                        batch_size += 1
-                    else
-                        same_batch_sup = false
+                        if i+j <= instance.nb_cars && instance.color_code[solution.sequence[i+j]] == batch_color && same_batch_sup
+                            batch_size += 1
+                        else
+                            same_batch_sup = false
+                        end
+                        j += 1
                     end
-                    j += 1
                 end
-
-                if batch_size >= instance.nb_paint_limitation
+                if batch_size > instance.nb_paint_limitation
                     cost_insertion[i] = Inf
                 else
                     solution_copy = deepcopy(solution)

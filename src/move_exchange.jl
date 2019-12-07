154b88611a2b72fe31f27659c84303c2707727f8
k-fretaud <49268413+k-fretaud@users.noreply.github.com>
2019-12-07 13:33:05 +0100

    Tests and batches for insertion

--- src/move_exchange.jl
+++ src/move_exchange.jl
@@ -25,10 +25,61 @@ function move_exchange!(solution::Solution, car_pos_a::Int,
         return solution
     end
 
-    #DEBUG and TODO: Comment / uncomment for debbuging.
-    #                Ensure the quality of the solution.
-    update_matrices!(solution, instance)
-    initialize_batches!(solution, instance)
+    # #DEBUG and TODO: Comment / uncomment for debbuging.
+    # #                Test the quality of the given solution.
+    # s1 = deepcopy(solution)
+    #
+    # update_matrices!(solution, instance)
+    # if !(solution.colors === nothing)
+    #     initialize_batches!(solution, instance)
+    # end
+    #
+    # error1 = false
+    # for i in 1:solution.length
+    #     bool_print = false
+    #     if s1.colors[i].start != solution.colors[i].start
+    #         println("\tBad start batch (",i,") - ", s1.colors[i].start," and ",solution.colors[i].start)
+    #         error1 = true
+    #         bool_print= true
+    #     end
+    #     if s1.colors[i].width != solution.colors[i].width
+    #         println("\tBad width batch (",i,") - ", s1.colors[i].width," and ",solution.colors[i].width)
+    #         error1 = true
+    #         bool_print= true
+    #     end
+    #     if bool_print
+    #         println("\t", instance.color_code[i] ,"-", instance.color_code[car_pos_a]," and ",instance.color_code[car_pos_b])
+    #     end
+    # end
+    #
+    # error2 = false
+    # for o in 1:instance.nb_HPRC+instance.nb_LPRC
+    #     for i in 1:solution.length
+    #         if s1.M1[o,i] != solution.M1[o,i]
+    #             error2 = true
+    #         end
+    #         if s1.M2[o,i] != solution.M2[o,i]
+    #             error2 = true
+    #         end
+    #         if s1.M3[o,i] != solution.M3[o,i]
+    #             error2 = true
+    #         end
+    #     end
+    # end
+    # if error1 || error2
+    #     println("\nIn move_exchange for ", car_pos_a, " and ", car_pos_b)
+    #     if error1
+    #         println("\nError batch")
+    #     end
+    #     if error2
+    #         println("\nError matrices")
+    #     end
+    # end
+
+    # #DEBUG and TODO: Comment / uncomment for debbuging.
+    # #                Ensure the quality of the solution.
+    # update_matrices!(solution, instance)
+    # initialize_batches!(solution, instance)
 
     car_a = solution.sequence[car_pos_a]
     car_b = solution.sequence[car_pos_b]
--- src/move_insertion.jl
+++ src/move_insertion.jl
@@ -15,10 +15,10 @@ Replaces car at `old_index` at `new_index` and updates the solution's matrices.
 function move_insertion!(solution::Solution, old_index::Int,
                          new_index::Int, instance::Instance)
 
-    #Update sequence
-    # car = solution.sequence[old_index]
-    # deleteat!(solution.sequence, old_index)
-    # insert!(solution.sequence, new_index, car)
+    # #DEBUG and TODO: Comment / uncomment for debbuging.
+    # #                Ensure the quality of the solution.
+    # update_matrices!(solution, instance)
+    # initialize_batches!(solution, instance)
 
     #Update sequence (better complexity ?)
     car_inserted = solution.sequence[old_index]
@@ -37,6 +37,60 @@ function move_insertion!(solution::Solution, old_index::Int,
 
     # TODO: COMPLEXITY NON-OPTIMAL
     update_matrices!(solution, instance)
+    if !(solution.colors === nothing)
+        initialize_batches!(solution, instance)
+    end
+
+    # #DEBUG and TODO: Comment / uncomment for debbuging.
+    # #                Test the quality of the move.
+    # s1 = deepcopy(solution)
+    #
+    # update_matrices!(solution, instance)
+    # if !(solution.colors === nothing)
+    #     initialize_batches!(solution, instance)
+    # end
+    #
+    # error1 = false
+    # for i in 1:solution.length
+    #     bool_print = false
+    #     if s1.colors[i].start != solution.colors[i].start
+    #         println("\tBad start batch (",i,") - ", s1.colors[i].start," and ",solution.colors[i].start)
+    #         error1 = true
+    #         bool_print= true
+    #     end
+    #     if s1.colors[i].width != solution.colors[i].width
+    #         println("\tBad width batch (",i,") - ", s1.colors[i].width," and ",solution.colors[i].width)
+    #         error1 = true
+    #         bool_print= true
+    #     end
+    #     if bool_print
+    #         println("\t", instance.color_code[i] ,"-", instance.color_code[old_index]," to ",instance.color_code[new_index])
+    #     end
+    # end
+    #
+    # error2 = false
+    # for o in 1:instance.nb_HPRC+instance.nb_LPRC
+    #     for i in 1:solution.length
+    #         if s1.M1[o,i] != solution.M1[o,i]
+    #             error2 = true
+    #         end
+    #         if s1.M2[o,i] != solution.M2[o,i]
+    #             error2 = true
+    #         end
+    #         if s1.M3[o,i] != solution.M3[o,i]
+    #             error2 = true
+    #         end
+    #     end
+    # end
+    # if error1 || error2
+    #     println("\nIn move_insertion for ", old_index, " to ", new_index)
+    #     if error1
+    #         println("\nError batch")
+    #     end
+    #     if error2
+    #         println("\nError matrices")
+    #     end
+    # end
 
     return solution
 end

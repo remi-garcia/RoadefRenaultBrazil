f5e62a5a3a7149d97d9d1323259344a2f0aee40e
Rémi <47191601+remi-garcia@users.noreply.github.com>
2019-12-11 16:35:11 +0100

    works

--- src/functions.jl
+++ src/functions.jl
@@ -175,7 +175,7 @@ function cost(solution::Solution, instance::Instance, objectives::BitArray{1})
     end
 
     if objectives[3]
-        if solution.colors === nothing || true
+        if solution.colors === nothing
             for i in (instance.nb_late_prec_day+2):solution.length
                 if instance.color_code[solution.sequence[i]] != instance.color_code[solution.sequence[i-1]]
                     cost_on_objective[3] += 1
